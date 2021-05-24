65a8e9075213e5fd88a0d04dd1e92d5fa75c6b9c
Msouza <msdsantos96@gmail.com>
2021-05-23 23:49:24 -0300

    database alter

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,25 +1,25 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-// interface IConnectionOptions {
-//   host: string;
-// }
+interface IConnectionOptions {
+  host: string;
+}
 
-export default async (): Promise<Connection> => {
-  const defaultOptions = await getConnectionOptions();
+// export default async (): Promise<Connection> => {
+//   const defaultOptions = await getConnectionOptions();
 
-  return createConnection(
-    Object.assign(defaultOptions, {
-      database:
-        process.env.NODE_ENV === 'test' ? 'database' : defaultOptions.database,
-    }),
-  );
-};
+//   return createConnection(
+//     Object.assign(defaultOptions, {
+//       database:
+//         process.env.NODE_ENV === 'test' ? 'mlgv_db' : defaultOptions.database,
+//     }),
+//   );
+// };
 
-// getConnectionOptions().then(options => {
-//   const newOptions = options as IConnectionOptions;
-//   newOptions.host = 'database'; // Esta opção deve estar exatamente com o mesmo nome definido no arquivo docker-compose.yml
+getConnectionOptions().then(options => {
+  const newOptions = options as IConnectionOptions;
+  newOptions.host = 'database'; // Esta opção deve estar exatamente com o mesmo nome definido no arquivo docker-compose.yml
 
-//   createConnection({
-//     ...options,
-//   });
-// });
+  createConnection({
+    ...options,
+  });
+});
