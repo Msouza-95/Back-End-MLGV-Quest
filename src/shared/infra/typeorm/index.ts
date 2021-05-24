24993e4896c88fe1a19bc0b381367fb2fbad983b
Msouza <msdsantos96@gmail.com>
2021-05-23 22:01:29 -0300

    database

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -6,7 +6,7 @@ interface IConnectionOptions {
 
 getConnectionOptions().then(options => {
   const newOptions = options as IConnectionOptions;
-  // newOptions.host = 'database'; // Esta opção deve estar exatamente com o mesmo nome definido no arquivo docker-compose.yml
+  newOptions.host = 'database'; // Esta opção deve estar exatamente com o mesmo nome definido no arquivo docker-compose.yml
 
   createConnection({
     ...options,
