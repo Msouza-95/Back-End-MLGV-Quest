9a99449e979ec6772d484d80a1ca8426aba344b5
Msouza <msdsantos96@gmail.com>
2021-05-27 08:37:59 -0300

    config db

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,14 +1,24 @@
-import { createConnection, getConnectionOptions } from 'typeorm';
+// import { createConnection, getConnectionOptions } from 'typeorm';
 
-interface IConnectionOptions {
-  host: string;
-}
+// interface IConnectionOptions {
+//   host: string;
+// }
 
-getConnectionOptions().then(options => {
-  const newOptions = options as IConnectionOptions;
-  newOptions.host = 'database'; // Esta opção deve estar exatamente com o mesmo nome definido no arquivo docker-compose.yml
+// getConnectionOptions().then(options => {
+//   const newOptions = options as IConnectionOptions;
+//   newOptions.host = 'database'; // Esta opção deve estar exatamente com o mesmo nome definido no arquivo docker-compose.yml
 
-  createConnection({
-    ...options,
-  });
+//   createConnection({
+//     ...options,
+//   });
+// });
+
+
+import { createConnection } from 'typeorm';
+
+
+export default createConnection().then(async conn => {
+  console.log('Connection established...');
+  
+  return conn;
 });
