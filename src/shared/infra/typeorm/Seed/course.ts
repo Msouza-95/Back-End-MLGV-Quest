30f142d68171b0a219a917bedb5c13647268cb20
Msouza <msdsantos96@gmail.com>
2021-07-03 07:19:27 -0300

    updade seeds

--- src/shared/infra/typeorm/Seed/course.ts
+++ src/shared/infra/typeorm/Seed/course.ts
@@ -3,6 +3,10 @@ import { v4 as uuid } from 'uuid';
 
 const code = uuid();
 const code2 = uuid();
+const code3 = uuid();
+const code4 = uuid();
+const code5 = uuid();
+const code6 = uuid();
 async function create() {
   const connection = await createConnection();
 
@@ -19,7 +23,7 @@ async function create() {
 
   await connection.query(
     ` INSERT INTO subject(code,title,course_id)
-    values('${code2}', 'IHC 1','49')
+    values('${code2}', 'SISTEMAS DISTRIBUÍDOS','${course[0].id}')
     `,
   );
 
@@ -42,6 +46,74 @@ async function create() {
   await connection.query(
     ` INSERT INTO class(subject_id,period_id )
     values('${subject[0].id}','${period[0].id}')
+    `,
+  );
+
+  await connection.query(
+    ` INSERT INTO subject(code,title,course_id)
+    values('${code3}', 'ÉTICA PROFISSIONAL ','${course[0].id}')
+    `,
+  );
+
+  const subject2 = await connection.query(
+    ` select * from  subject where code = '${code3}'
+    `,
+  );
+
+  await connection.query(
+    ` INSERT INTO class(subject_id,period_id )
+    values('${subject2[0].id}','${period[0].id}')
+    `,
+  );
+
+  await connection.query(
+    ` INSERT INTO subject(code,title,course_id)
+    values('${code4}', 'LABORATÓRIO DE DESENVOLVIMENTO WEB ','${course[0].id}')
+    `,
+  );
+
+  const subject3 = await connection.query(
+    ` select * from  subject where code = '${code4}'
+    `,
+  );
+
+  await connection.query(
+    ` INSERT INTO class(subject_id,period_id )
+    values('${subject3[0].id}','${period[0].id}')
+    `,
+  );
+
+  await connection.query(
+    ` INSERT INTO subject(code,title,course_id)
+    values('${code5}', 'INTERFACE HUMANO - COMPUTADOR','${course[0].id}')
+    `,
+  );
+
+  const subject4 = await connection.query(
+    ` select * from  subject where code = '${code5}'
+    `,
+  );
+
+  await connection.query(
+    ` INSERT INTO class(subject_id,period_id )
+    values('${subject4[0].id}','${period[0].id}')
+    `,
+  );
+
+  await connection.query(
+    ` INSERT INTO subject(code,title,course_id)
+    values('${code6}', 'PROJETO AVANÇADO DE SISTEMAS','${course[0].id}')
+    `,
+  );
+
+  const subject5 = await connection.query(
+    ` select * from  subject where code = '${code6}'
+    `,
+  );
+
+  await connection.query(
+    ` INSERT INTO class(subject_id,period_id )
+    values('${subject5[0].id}','${period[0].id}')
     `,
   );
 
