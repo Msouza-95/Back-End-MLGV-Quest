cdd19082a7570101f456c9e04f1401a8c5596257
Msouza <msdsantos96@gmail.com>
2021-07-05 07:06:22 -0300

    rt

--- src/modules/user/infra/http/middleware/ensureAuthenticated.ts
+++ src/modules/user/infra/http/middleware/ensureAuthenticated.ts
@@ -14,20 +14,25 @@ export default function ensureAuthenticated(
   next: NextFunction,
 ): void {
   const authHeader = request.headers.authorization;
-  // if (!authHeader) {
-  //   throw new AppError('Token missing', 401);
-  // }
-  // const [, token] = authHeader.split(' ');
-  // try {
-  //   const { sub: user_id } = verify(token, authConfig.jwt.secret) as IPayload;
-  //   request.user = {
-  //     id: user_id,
-  //   };
-  //   if (!request.user) {
-  //     throw new AppError('user does not exists');
-  //   }
-  next();
-  // } catch {
-  //   throw new AppError('Invalid token', 401);
-  // }
+
+  if (!authHeader) {
+    throw new AppError('Token missing', 401);
+  }
+
+  const [, token] = authHeader.split(' ');
+  try {
+    const { sub: user_id } = verify(token, authConfig.jwt.secret) as IPayload;
+
+    request.user = {
+      id: user_id,
+    };
+
+    if (!request.user) {
+      throw new AppError('user does not exists');
+    }
+
+    next();
+  } catch {
+    throw new AppError('Invalid token', 401);
+  }
 }
