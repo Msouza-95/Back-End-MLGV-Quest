9d0f41464c7bca8573bb97d51737035333e57b2e
Msouza <msdsantos96@gmail.com>
2021-07-05 18:36:16 -0300

    ms

--- src/modules/exam/infra/typeorm/repositories/ExamRepository.ts
+++ src/modules/exam/infra/typeorm/repositories/ExamRepository.ts
@@ -50,8 +50,9 @@ class ExamRepository implements IExamRepository {
 
   public async queryCSV(exam_id: number): Promise<any> {
     const Query = this.ormRepository.query(
-      'select agree.exam_id as exam_id, agree.id as agreement_id, agree.comment as Comentario, agree.uuid as Codigo, qg.title as group_title, qg.class as is_class, qg.id as group_id, q.id as question_id, q.statement as Questao, a.id as answer_id,  a.score as score,  s.title as Disciplina,  u.enrollment as Matricula,  u.email as Usuarios FROM userAgreement  as agree INNER JOIN user as  u ON u.id = agree.user_id  INNER JOIN userAnswer as a ON a.user_agreement_id = agree.id INNER JOIN userAnswerClass as uclass ON a.id = uclass.user_answer_id LEFT JOIN class as g ON g.id = uclass.class_id  LEFT JOIN subject as s ON s.id = g.subject_id INNER JOIN question as q ON q.id = a.question_id INNER JOIN examQuestionGroup  as ex  ON ex.id = q.exam_question_group_id  INNER JOIN questionGroup as qg ON qg.id = ex.question_group_id WHERE agree.exam_id = ? ORDER BY  agree.id ASC, qg.id ASC, q.id ASC',
-      [exam_id],
+      'select agree.exam_id as exam_id, agree.id as agreement_id, agree.comment as Comentario, agree.uuid as Codigo, qg.title as group_title, qg.class as is_class, qg.id as group_id, q.id as question_id, q.statement as Questao, a.id as answer_id, a.score as score, s.title as Disciplina, u.enrollment as Matricula, u.email as Usuarios FROM userAgreement as agree INNER JOIN user as u ON u.id = agree.user_id INNER JOIN userAnswer as a ON a.user_agreement_id = agree.id INNER JOIN userAnswerClass as uclass ON a.id = uclass.user_answer_id LEFT JOIN class as g ON g.id = uclass.class_id LEFT JOIN subject as s ON s.id = g.subject_id INNER JOIN question as q ON q.id = a.question_id INNER JOIN examQuestionGroup as ex ON ex.id = q.exam_question_group_id INNER JOIN questionGroup as qg ON qg.id = ex.question_group_id WHERE agree.exam_id =? ORDER BY agree.id ASC, qg.id ASC, q.id ASC'[
+        exam_id
+      ],
     );
 
     return Query;
