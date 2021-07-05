cb33e643cf5edacaf919d04cc48c9edcfc3f98d0
Msouza <msdsantos96@gmail.com>
2021-07-05 15:52:26 -0300

    ts

--- src/modules/answer/services/CreateUserAnswerService.ts
+++ src/modules/answer/services/CreateUserAnswerService.ts
@@ -74,7 +74,7 @@ class CreateUserAnswerService {
 
         const userAnswerClass = await this.userAnswerClassRepository.create({
           user_answer_id: userAnswer.id,
-          class_id: 19, //
+          class_id: 7, // 19 serve 7 localhost
           score: wer.score,
         });
       } else {
